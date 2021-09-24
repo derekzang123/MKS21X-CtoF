c0d154e4ca1c34748bd6d20f5d925121e3cc6ce4
Derek Zang <dzang30@stuy.edu>
2021-09-24 07:24:10 -0400

    created comments to answer questions/plan work

---
+++ AP Comp Sci/CtoFTester.java
@@ -0,0 +1,6 @@
+// What should the functions have as parameter(s)?
+//    Parameters should be double farenheit and double celsius
+// What type should they be?
+//    they should be double (we need decimals, prob not a lot though)
+// What should the functions return?
+//    the opposite, so cels > faren return faren and faren > cels return cels
