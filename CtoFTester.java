dd0179c974fe2a797a4c43a4acee48334283e90f
Derek Zang <dzang30@stuy.edu>
2021-10-24 22:56:54 -0400

    Created fahrenheitToCelsius method

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,7 +1,13 @@
 public class CtoFTester {
+
   public static float celsiusToFahrenheit(float C) {
     return (9/5) * C + 32;
   }
 
-  
+  public static float fahrenheitToCelsius(float F) {
+    return ((F - 32)*5)/9;
+  }
+
+
+
 }
