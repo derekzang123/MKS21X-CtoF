5d429599bf18d0149b0252d57f700468f65994e0
Derek Zang <dzang30@stuy.edu>
2021-10-24 23:01:58 -0400

    Added comments and tests (main)

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,15 @@
+/*
+What should the functions have as parameter(s)?
+- celsiusToFahrenheit should have a parameter "C" for "celsius"
+- fahrenheitToCelsius should have a parameter "F" for "fahrenheit"
+
+What type should they be?
+- Should be a float
+
+What should the functions return?
+- Should return floats
+*/
+
 public class CtoFTester {
 
   public static float celsiusToFahrenheit(float C) {
@@ -6,6 +18,19 @@ public class CtoFTester {
 
   public static float fahrenheitToCelsius(float F) {
     return ((F - 32)*5)/9;
+  }
+
+  public static void main (String[] args) {
+
+    // Tests for celsiusToFahrenheit
+    System.out.println(celsiusToFahrenheit(0));
+    System.out.println(celsiusToFahrenheit(5));
+    System.out.println(celsiusToFahrenheit(-5));
+
+    // Tests for fahrenheitToCelsius
+    System.out.println(fahrenheitToCelsius(32));
+    System.out.println(fahrenheitToCelsius(0));
+    System.out.println(fahrenheitToCelsius(41));
   }
 
 
